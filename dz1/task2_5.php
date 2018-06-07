4ecbf26bade4d7e103a3fe8129ea6266e95dba03
Dmitry <threed@rsgc.ru>
2018-06-07 16:12:10 +0300

    DZ1 task 2.5

--- dz1/task2_5.php
+++ dz1/task2_5.php
@@ -1,7 +1,48 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Дмитрий
- * Date: 07.06.2018
- * Time: 02:41 PM
+ * Метод проверки правильности расстановки скобочек с помощью стека.
+ * @param $str
+ * @return bool|int
  */
+function check_braces($str) {
+    $braces = ['(' => ')', '[' => ']', '{' => '}' ];
+    $stack = new SplStack();
+
+    for ($i = 0; $i < strlen($str); $i++)
+    {
+        foreach ($braces as $ob => $cb) {
+            // Открывающая скобка
+            if ($ob == $str[$i]) {
+                $stack->push($cb);
+                continue 2;
+            }
+
+            // Закрывающая скобка
+            if ($cb == $str[$i]) {
+                if (!$stack->count())
+                    return false; // Стек пустой
+
+                if ($stack->pop() != $cb) // Закрывающая скобка отличается
+                    return false;
+                continue 2;
+            }
+        }
+    }
+
+    return !$stack->count(); // Стек должен остаться пустым
+}
+
+/**
+ * @param $b
+ * @return string
+ */
+function bool_str($b) {
+    return $b?"true":"false";
+}
+
+
+echo "([]){} = ".bool_str(check_braces("([]){}")).PHP_EOL;
+echo "([){} = ".bool_str(check_braces("([){}")).PHP_EOL;
+echo "( = ".bool_str(check_braces("(")).PHP_EOL;
+echo "([)] = ".bool_str(check_braces("([)]")).PHP_EOL;
+echo "{(2+2)*(2)} = ".bool_str(check_braces("{(2+2)*(2)}")).PHP_EOL;
