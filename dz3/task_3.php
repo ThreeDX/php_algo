813c961dda52a7a393b7045dbb2902a14932337c
Dmitry <threed@rsgc.ru>
2018-06-18 18:36:01 +0300

    DZ3 task 3 fix li ul

--- dz3/task_3.php
+++ dz3/task_3.php
@@ -1,8 +1,4 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "753159", "php_algo");
-$res = $mysqli->query("select c.`category_name`, nl.* from `categories` c join `nested_links` nl on nl.`category_id` = c.`id_category` order by nl.`left_key`");
-
-$menu = $res->fetch_all(MYSQLI_ASSOC);
 
 function build_html(array $menu)
 {
@@ -12,18 +8,29 @@ function build_html(array $menu)
         if ($item['level'] > $level) {
             $html .= "<ul>";
         } else if ($item['level'] < $level) {
-            $html .= str_repeat('</ul>', $level - $item['level']);
+            $html .= str_repeat('</li></ul>', $level - $item['level'])."</li>";
+        } else {
+            $html .= "</li>";
         }
 
         $level = $item['level'];
-        $html .= "<li>{$item['category_name']}</li>";
+        $html .= "<li>{$item['category_name']}";
 
         //echo str_repeat('.', $item['level']) . " " . $item['category_name'] . PHP_EOL;
     }
     if ($level)
-        $html .= str_repeat('</ul>', $level);
+        $html .= str_repeat('</li></ul>', $level);
 
     return $html;
 }
 
+
+$mysqli = new mysqli("localhost", "root", "753159", "php_algo");
+$res = $mysqli->query("select c.`category_name`, nl.* from `categories` c join `nested_links` nl on nl.`category_id` = c.`id_category` order by nl.`left_key`");
+
+$menu = $res->fetch_all(MYSQLI_ASSOC);
+
 echo build_html($menu);
+
+
+
